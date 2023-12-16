7652844445f94381a7efb6920c40c3497014b20b
MichaelRaceUp <149481035+MichaelRaceUp@users.noreply.github.com>
2023-12-16 15:02:17 +0100

    bumblebee2
    upload bumblebee directory and README.md file with the explanation of how to use it

---
+++ bumblebee2_ros_driver/src/test_bumblebee2.cpp
@@ -0,0 +1,55 @@
+#include <memory>
+#include <opencv2/opencv.hpp>
+
+#include <rclcpp/rclcpp.hpp>
+#include "bumblebee2_ros_driver/msg/stereo_image.hpp"
+
+#include "bumblebee2_driver.h"
+
+using std::placeholders::_1;
+
+class TestBumblebee2 : public rclcpp::Node
+{
+public:
+  TestBumblebee2 ()
+  : Node("test_bumblebee2")
+  {
+    subscription_ = this->create_subscription<bumblebee2_ros_driver::msg::StereoImage>(
+      "stereo_image", 10, std::bind(&TestBumblebee2 ::topicCallback, this, _1));
+    
+    printf ( "%c\n", 'a' );
+    cv::namedWindow("left");
+    cv::namedWindow("right");
+  }
+
+private:
+  void topicCallback(const bumblebee2_ros_driver::msg::StereoImage::SharedPtr msg) const
+  {
+    printf ( "%c\n", 'b' );
+    
+    
+    cv::Mat_<uchar> left_img(msg->height, msg->width, msg->left_data.data()),
+                        right_img(msg->height, msg->width, msg->right_data.data());
+                        
+    cv::Mat_<cv::Vec3b> left_img_rgb(left_img.rows, left_img.cols),
+                        right_img_rgb(right_img.rows, right_img.cols);
+
+    bumblebee2Debayer(left_img, left_img_rgb);
+    bumblebee2Debayer(right_img, right_img_rgb);
+
+    printf ( "%c\n", 'c' );
+    cv::imshow("left", left_img_rgb);
+    cv::imshow("right", right_img_rgb);
+    cv::waitKey(10);
+  }
+  
+  rclcpp::Subscription<bumblebee2_ros_driver::msg::StereoImage>::SharedPtr subscription_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<TestBumblebee2>());
+  rclcpp::shutdown();
+  return 0;
+}
